dd61eadcde9800f3980b22298df4c9b729e5d7de
Giorgi <giorgiquchuloria7@gmail.com>
2024-04-25 23:48:58 +0400

    changed views include

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mainRoute        = require('./routes/web_route.js')
 const bookRoute        = require('./routes/book_route.js')
 const userRoute        = require('./routes/user_route.js')
 const ejs              = require('ejs')
+const path             = require('path')
 const { PrismaClient } = require('@prisma/client');
 const session          = require('express-session')
 const passport         =require('passport')
@@ -13,7 +14,7 @@ const prisma           = new PrismaClient();
 app.use(express.json())
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.static('views'))
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}))
 app.use(session({ secret: 'your_secret_key', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
