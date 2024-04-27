911571a28514cd4f412ac0dbf17ff2b37baea5b1
Giorgi <giorgiquchuloria7@gmail.com>
2024-04-27 15:08:04 +0400

    added google oauth code

--- config/passport-config.js
+++ config/passport-config.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const bcrypt = require('bcrypt');
 const { PrismaClient } = require('@prisma/client');
+const { v4: uuidv4 } = require('uuid')
 const prisma = new PrismaClient();
 
 passport.use(
@@ -10,13 +12,11 @@ passport.use(
       const user = await prisma.users.findUnique({ where: { email } });
 
       if (!user) {
-        // req.flash('error', 'Incorrect email.');
         return done(null, false, { message: 'Incorrect email or password.' });
       }
 
       const isPasswordValid = await bcrypt.compare(password, user.password);
       if (!isPasswordValid) {
-
         return done(null, false, { message: 'Incorrect email or password.' });
       }
 
@@ -27,15 +27,41 @@ passport.use(
   })
 );
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      const user = await prisma.users.findUnique({ where: { email: profile.emails[0].value } });
+
+      if (!user) {
+        const newUser = await prisma.users.create({
+          data: {
+            email: profile.emails[0].value,
+            full_name: profile.displayName,
+            isVerified: true,
+            password: uuidv4()
+          }
+        });
+        return done(null, newUser);
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }
+));
+
 passport.serializeUser((user, done) => {
   done(null, user.id); 
 });
 
 passport.deserializeUser(async (id, done) => {
   try {
-
     const user = await prisma.users.findUnique({ where: { id: parseInt(id) } });
-
     done(null, user);
   } catch (err) {
     done(err);
