7b4516845a86ee3a8ce795923d757829640721cc
Giorgi <giorgiquchuloria7@gmail.com>
2024-05-29 17:55:25 +0400

    added timestamp

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,14 +1,17 @@
 const mongoose  = require('mongoose')
 
-const dataSchema =  new mongoose.Schema({
+const dataSchema = new mongoose.Schema({
     sessionId: {
         type: String,
     },
     userId: {
         type: String
     },
-    deviceInfo: {
+    deviceName: {
         type: String,
+    },
+    browserName: {
+        type: String
     },
     ipAddress: {
         type: String
@@ -16,7 +19,9 @@ const dataSchema =  new mongoose.Schema({
     allowance: {
         type: String
     }
-})
+}, {
+    timestamps: true 
+});
 
 const User = mongoose.model('User', dataSchema)
 module.exports = {User}
