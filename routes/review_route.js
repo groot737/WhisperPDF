80e5a203b95dd0803e4b69f4ca36e8989e5097b1
Giorgi <giorgiquchuloria7@gmail.com>
2024-06-07 02:26:36 +0400

    added auth security

--- routes/review_route.js
+++ routes/review_route.js
@@ -9,30 +9,30 @@ require('dotenv').config();
 
 //create
 router.post('/add', async (req, res) => {
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         const { rating, description, bookId } = req.body;
-    try {
-        const book = await prisma.pdfBook.findUnique({
-            where: { id: +bookId }
-        });
-        if (!book) {
-            return res.status(500).json({ message: "Book not found" });
-        }
-        const review = await prisma.Review.create({
-            data: {
-                book_id: +bookId,
-                user_id: +req.user.id,
-                description: description,
-                rating: +rating
+        try {
+            const book = await prisma.pdfBook.findUnique({
+                where: { id: +bookId }
+            });
+            if (!book) {
+                return res.status(500).json({ message: "Book not found" });
             }
-        });
-        res.json("Review added");
-    } catch (error) {
-        console.error("Error adding review:", error);
-        res.status(500).json({ message: "Error occurred while adding review" });
-    }
-    } else{
-        res.status(401).json({message: "you should log in to account"})
+            const review = await prisma.Review.create({
+                data: {
+                    book_id: +bookId,
+                    user_id: +req.user.id,
+                    description: description,
+                    rating: +rating
+                }
+            });
+            res.json("Review added");
+        } catch (error) {
+            console.error("Error adding review:", error);
+            res.status(500).json({ message: "Error occurred while adding review" });
+        }
+    } else {
+        res.status(401).json({ message: "you should log in to account" })
     }
 });
 
@@ -50,7 +50,7 @@ router.get('/:id', async (req, res) => {
             return res.status(404).json({ message: "No reviews found for this book" });
         }
 
-        let data = [{total_review: 0, average_rating: 0}, []];
+        let data = [{ total_review: 0, average_rating: 0 }, []];
         let counter = 0
         let sum = 0
         for (let j = 0; j < reviews.length; j++) {
@@ -64,7 +64,7 @@ router.get('/:id', async (req, res) => {
             // Add username and profile_pic to the review
             reviews[j]['username'] = user.full_name;
             reviews[j]['profile_pic'] = user.profile_pic;
-            counter ++
+            counter++
             sum += reviews[j].rating
 
             data[1].push(reviews[j]);
@@ -83,57 +83,57 @@ router.get('/:id', async (req, res) => {
 
 
 // update
-router.patch('/update', async(req, res) =>{
-   if(req.isAuthenticated){
-    const reviewId = +req.body.reviewId; 
-    const { description } = req.body;
+router.patch('/update', async (req, res) => {
+    if (req.isAuthenticated) {
+        const reviewId = +req.body.reviewId;
+        const { description } = req.body;
 
-    if (!description) {
-        return res.status(400).json({ message: 'Description is required' });
-    }
-
-    try {
-        const existingReview = await prisma.review.findUnique({
-            where: { id: reviewId }
-        });
-
-        if (!existingReview) {
-            return res.status(404).json({ message: 'Review not found' });
+        if (!description) {
+            return res.status(400).json({ message: 'Description is required' });
         }
 
-        const updatedReview = await prisma.review.update({
-            where: { id: reviewId },
-            data: { description: description }
-        });
+        try {
+            const existingReview = await prisma.review.findUnique({
+                where: { id: reviewId }
+            });
 
-        res.json(updatedReview);
-    } catch (error) {
-        console.error('Error updating review:', error);
-        res.status(500).json({ message: 'Error occurred while updating review' });
+            if (!existingReview) {
+                return res.status(404).json({ message: 'Review not found' });
+            }
+
+            const updatedReview = await prisma.review.update({
+                where: { id: reviewId },
+                data: { description: description }
+            });
+
+            res.json(updatedReview);
+        } catch (error) {
+            console.error('Error updating review:', error);
+            res.status(500).json({ message: 'Error occurred while updating review' });
+        }
+    } else {
+        res.status(401).json({ message: "you should log in to account" })
     }
-   } else {
-    res.status(401).json({message: "you should log in to account"})
-   }
 })
 
 // delete 
 router.delete('/delete', async (req, res) => {
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         const { reviewId } = req.body;
-    try {
-        const deletedReview = await prisma.Review.delete({
-            where: { id: +reviewId }
-        });
-        if (!deletedReview) {
-            return res.status(404).json({ message: "Review not found" });
+        try {
+            const deletedReview = await prisma.Review.delete({
+                where: { id: +reviewId }
+            });
+            if (!deletedReview) {
+                return res.status(404).json({ message: "Review not found" });
+            }
+            res.json({ message: "Review deleted" });
+        } catch (error) {
+            console.error("Error deleting review:", error);
+            res.status(500).json({ message: "Error occurred while deleting review" });
         }
-        res.json({message: "Review deleted"});
-    } catch (error) {
-        console.error("Error deleting review:", error);
-        res.status(500).json({ message: "Error occurred while deleting review" });
-    }
-    } else{
-        res.status(401).json({message: "you should log in to account"})
+    } else {
+        res.status(401).json({ message: "you should log in to account" })
     }
 });
 
